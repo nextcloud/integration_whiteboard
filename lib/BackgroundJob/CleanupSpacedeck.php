5c1a9ee1b01d21dfb6471cba0e4c28d746727860
Julien Veyssier <eneiluj@posteo.net>
2021-02-10 15:18:42 +0100

    add daily background job to cleanup spacedeck storage/db
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/BackgroundJob/CleanupSpacedeck.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Julien Veyssier <eneiluj@posteo.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Spacedeck\BackgroundJob;
+
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\TimedJob;
+use OCP\IConfig;
+use Psr\Log\LoggerInterface;
+
+use OCA\Spacedeck\Service\SpacedeckAPIService;
+use OCA\Spacedeck\AppInfo\Application;
+
+require_once __DIR__ . '/../constants.php';
+
+/**
+ * Class CleanupSpacedeck
+ *
+ * @package OCA\Spacedeck\BackgroundJob
+ */
+class CleanupSpacedeck extends TimedJob {
+
+	/** @var IConfig */
+	protected $config;
+
+	/** @var SpacedeckAPIService */
+	protected $apiService;
+
+	/** @var LoggerInterface */
+	protected $logger;
+
+	public function __construct(ITimeFactory $time,
+								IConfig $config,
+								SpacedeckAPIService $apiService,
+								LoggerInterface $logger) {
+		parent::__construct($time);
+
+		// Every 24 hours
+		$this->setInterval(60 * 60 * 24);
+
+		$this->config = $config;
+		$this->apiService = $apiService;
+		$this->logger = $logger;
+	}
+
+	protected function run($argument): void {
+		$apiToken = $this->config->getAppValue(Application::APP_ID, 'api_token', DEFAULT_SPACEDECK_API_KEY);
+		$apiToken = $apiToken ?: DEFAULT_SPACEDECK_API_KEY;
+		$baseUrl = $this->config->getAppValue(Application::APP_ID, 'base_url', DEFAULT_SPACEDECK_URL);
+		$baseUrl = $baseUrl ?: DEFAULT_SPACEDECK_URL;
+
+        $result = $this->apiService->cleanupSpacedeckStorage($baseUrl, $apiToken);
+        if (isset($result['error'])) {
+			$this->logger->error('[ERROR] ' . $result['error']);
+        } elseif (isset($result['actions'])) {
+            foreach ($result['actions'] as $action) {
+				$this->logger->info($action);
+            }
+        }
+	}
+}
